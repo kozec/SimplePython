ed34855e85425945887fb46ea33ae4db8a3d7adc
EnerccioCLI <enerccio@paldopice.com>
2015-08-09 13:22:36 +0200

    fixed yield and generators

--- src/me/enerccio/sp/interpret/PythonInterpreter.java
+++ src/me/enerccio/sp/interpret/PythonInterpreter.java
@@ -573,8 +573,9 @@ public class PythonInterpreter extends PythonObject {
 			break;
 		case RETURN:
 			// removes the frame and returns value
-			if (o.ownedGenerator != null && !o.yielding)
-				throw Utils.throwException("StopIteration");
+			if (o.ownedGenerator != null)
+				if (!o.yielding)
+					throw Utils.throwException("StopIteration");
 			if (o.nextInt() == 1) {
 				o.returnHappened = true;
 				PythonObject retVal = stack.pop();
@@ -822,7 +823,7 @@ public class PythonInterpreter extends PythonObject {
 				List<FrameObject> ol = new ArrayList<FrameObject>();
 				FrameObject oo = o;
 				while (oo != null){
-					ol.add(oo);
+					ol.add(oo.cloneFrame());
 					oo = oo.parentFrame;
 				}
 				Collections.reverse(ol);
