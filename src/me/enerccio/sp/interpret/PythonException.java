e3ff2acf172475143280a4bf316c02cd335322d6
kozec <kozec2@kozec.com>
2015-08-10 10:51:49 +0200

    Moved StackElement to own .java file

--- src/me/enerccio/sp/interpret/PythonException.java
+++ src/me/enerccio/sp/interpret/PythonException.java
@@ -17,12 +17,6 @@
  */
 package me.enerccio.sp.interpret;
 
-import java.util.Map;
-import java.util.Set;
-
-import me.enerccio.sp.runtime.ModuleInfo;
-import me.enerccio.sp.types.PythonObject;
-import me.enerccio.sp.types.callables.JavaMethodObject;
 
 /**
  * Represents root exception that can be raised by SimplePython
@@ -39,51 +33,4 @@ public class PythonException extends RuntimeException {
 	public PythonException(){
 		super();
 	}
-	
-
-	public static final StackElement SYSTEM_FRAME = new StackElement(); 
-	public static final StackElement LAST_FRAME = new StackElement();
-
-	/** Container for stack data */
-	public static class StackElement extends PythonObject {
-		private static final long serialVersionUID = -3288411350030175582L;
-		public final ModuleInfo module;
-		public final int line;
-		public final int character;
-		public final String function;
-		
-		private StackElement() {
-			line = character = -1;
-			module = null;
-			function = "<module>";
-		}
-
-		public StackElement(ModuleInfo module, String function, int line, int character) {
-			this.module = module;
-			this.line = line;
-			this.character = character;
-			this.function = function;
-		}
-
-		@Override
-		public boolean truthValue() {
-			return true;
-		}
-
-		@Override
-		protected String doToString() {
-			return "<" + module.getName() + " at line " + line + " " + character + ">"; 
-		}
-		
-		@Override
-		public Set<String> getGenHandleNames() {
-			return PythonObject.sfields.keySet();
-		}
-
-		@Override
-		protected Map<String, JavaMethodObject> getGenHandles() {
-			return PythonObject.sfields;
-		}
-	}
-
 }
