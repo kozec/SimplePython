a5a1710817ec97dbd1b0a64c5035927f62df2291
kozec <kozec@kozec.com>
2015-08-12 12:18:31 +0200

    Added causes to PythonExceptions

--- src/me/enerccio/sp/errors/TypeError.java
+++ src/me/enerccio/sp/errors/TypeError.java
@@ -5,7 +5,11 @@ import me.enerccio.sp.runtime.PythonRuntime;
 public class TypeError extends PythonException {
 	private static final long serialVersionUID = 9845123L;
 	
+	public TypeError(String message, Throwable cause) {
+		super(PythonRuntime.TYPE_ERROR, message, cause);
+	}
+	
 	public TypeError(String message) {
-		super(PythonRuntime.TYPE_ERROR, message);
+		super(PythonRuntime.TYPE_ERROR, message, null);
 	}
 }
--- src/me/enerccio/sp/errors/ValueError.java
+++ src/me/enerccio/sp/errors/ValueError.java
@@ -6,6 +6,6 @@ public class ValueError extends PythonException {
 	private static final long serialVersionUID = 84512156121L;
 	
 	public ValueError(String message) {
-		super(PythonRuntime.VALUE_ERROR, message);
+		super(PythonRuntime.VALUE_ERROR, message, null);
 	}
 }
