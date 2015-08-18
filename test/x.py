9136d33026aec7a63c36a2635d8f3757c348d0b5
EnerccioCLI <enerccio@paldopice.com>
2015-08-18 12:11:03 +0200

    optimization of interprete pass 1, refactor into small functions

--- test/x.py
+++ test/x.py
@@ -1,22 +1,9 @@
-class X(object):
-    def __init__(self):
-        self.ec = 0
-    def __enter__(self):
-        try:
-            return self.ec
-        finally:
-            self.ec += 1
-    
-    def __exit__(self, type, value):
-        print type, value
-        
+def x():
+    c = 0
+    for a in xrange(1024):
+        for b in xrange(1024):
+            c += a + b
+    return c 
+
 def test():
-    x = X()
-    for a in (0, 1):
-        try:
-            with x as y:
-                print y
-                break
-        finally:
-            print "b"
-        print "a"
+    print x()
